768901d56e1563560fb902deadcd80b342610b04
danielsouza <eu@danielsouza.dev.br>
2021-10-21 10:54:32 -0300

    .

---
+++ resources/js/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+
+createInertiaApp({
+  resolve: (name) => require(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    render(<App {...props} />, el);
+  },
+});
+
+InertiaProgress.init();
---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import 'bootstrap';
+import { InertiaProgress } from '@inertiajs/progress';
+
+import './bootstrap';
+import './App';
+
+InertiaProgress.init();
